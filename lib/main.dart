aed46f4b68d2b88d1eafdafd13a74a9c292715a3
Prashant Ranjan singh <prashant.singh.123123452@gmail.com>
2024-07-16 15:35:00 +0530

    Latest Release

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:prashant_potfolio/view/splash_screen/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
