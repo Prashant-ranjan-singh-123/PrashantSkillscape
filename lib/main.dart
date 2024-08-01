e063c1c859c18dd08d0c2386cbdb54de2d11dc6a
prashantranjansingh <prashant.singh.12312345@gmail.com>
2024-08-01 23:01:43 +0530

    final release

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.white,
           brightness: Brightness.light,
