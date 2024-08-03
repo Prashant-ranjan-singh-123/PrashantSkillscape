9f6b7af0c36b1b5511a448993b64a492b686e67c
prashantranjansingh <prashant.singh.12312345@gmail.com>
2024-08-03 16:06:26 +0530

    Files Changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: Typography.whiteMountainView,
       ),
+
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.white,
